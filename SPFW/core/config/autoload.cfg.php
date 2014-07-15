989f9569dc81f63107b16855e99262a2bbc851bb
Jerryli <hzjerry@gmail.com>
2014-07-15 16:03:41 +0800

    增加了中文转换拼音类

--- SPFW/core/config/autoload.cfg.php
+++ SPFW/core/config/autoload.cfg.php
@@ -21,8 +21,6 @@ return array
 	array('package'=>'core.lib.sys',	'file'=>'CExtModule.class.php'),
 'CExtendManage'=> //Xml结构数组节点管理类
 	array('package'=>'core.lib.sys',	'file'=>'CExtendManage.class.php'),
-'CSeaPhpWebServiceClient'=> //Sea PHP架构的 Web Service专用客户端
-	array('package'=>'core.lib.sys',	'file'=>'CSeaPhpWebServiceClient.class.php'),
 
 'CDate'=> //时间日期处理类
 	array('package'=>'core.lib.final',	'file'=>'CDate.class.php'),
@@ -44,11 +42,13 @@ return array
 	array('package'=>'core.lib.final',	'file'=>'CErrThrow.class.php'),
 'CEncryption'=> //加密压缩传输
 	array('package'=>'core.lib.final',	'file'=>'CEncryption.class.php'),
+'CUtf8_PY'=> //PHP 汉字转拼音
+	array('package'=>'core.lib.final',	'file'=>'CUtf8_PY.class.php'),
 
 /*调试使用，正式平台上删除以下内容*/
-'CJsonArrayConver'=> //Json与Php的XML结构数组双向映射处理
-	array('package'=>'extend.webservice.lib',	'file'=>'CJsonArrayConver.class.php'),
-'CXmlArrayConverGET'=> //Xml结构数组与Http的GET请求数据转换
-	array('package'=>'extend.webservice.lib',	'file'=>'CXmlArrayConverGET.class.php'),
+// 'CJsonArrayConver'=> //Json与Php的XML结构数组双向映射处理
+// 	array('package'=>'extend.webservice.lib',	'file'=>'CJsonArrayConver.class.php'),
+// 'CXmlArrayConverGET'=> //Xml结构数组与Http的GET请求数据转换
+// 	array('package'=>'extend.webservice.lib',	'file'=>'CXmlArrayConverGET.class.php'),
 );
 ?>
